39284745db1a3c2e067302805888bb1a3e2b5595
SEKI-YUTA <zard.since1991@gmail.com>
2024-08-04 14:25:14 +0900

    最初の構成でホーム画面、ランキング画面、自分の記録画面、設定画面を作成

---
+++ client/composeApp/src/commonMain/kotlin/screen/MyLogScreen.kt
@@ -0,0 +1,42 @@
+package screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import navigation.BottomNavigationBar
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MyLogScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+) {
+    Scaffold(
+        modifier = modifier,
+        bottomBar = {
+             BottomNavigationBar(navController = navController)
+        }
+    ) {
+       Box(
+           modifier = Modifier
+               .padding(it)
+               .fillMaxSize(),
+           contentAlignment = Alignment.Center
+       ) {
+           Text("MyLog Screen")
+       }
+    }
+}
+
+@Preview
+@Composable
+fun MyLogScreenPreview() {
+    MyLogScreen(navController = rememberNavController())
+}
---
+++ client/composeApp/src/commonMain/kotlin/screen/RankingScreen.kt
@@ -0,0 +1,42 @@
+package screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import navigation.BottomNavigationBar
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RankingScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+) {
+    Scaffold(
+        modifier = modifier,
+        bottomBar = {
+            BottomNavigationBar(navController = navController)
+        }
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Ranking Screen")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun RankingScreenPreview() {
+    RankingScreen(navController = rememberNavController())
+}
