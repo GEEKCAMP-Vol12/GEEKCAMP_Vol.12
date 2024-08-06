387e78d28cedeb38f918e8bb737e60ef615d1cfc
SEKI-YUTA <zard.since1991@gmail.com>
2024-08-06 20:13:12 +0900

    ホーム画面のUIを作成

--- client/composeApp/src/commonMain/kotlin/screen/HomeScreen.kt
+++ client/composeApp/src/commonMain/kotlin/screen/HomeScreen.kt
@@ -1,28 +1,63 @@
 package screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import di.SampleRepository
+import healthapp.composeapp.generated.resources.Res
+import healthapp.composeapp.generated.resources.character_good
+import healthapp.composeapp.generated.resources.room_sunny
 import navigation.BottomNavigationBar
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.koin.compose.koinInject
 
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
     navController: NavController,
 ) {
-    val sampleRepository = koinInject<SampleRepository>()
     Scaffold(
         modifier = modifier,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+
+                },
+                content = {
+                    Icon(Icons.Rounded.Add, contentDescription = "Add Log")
+                },
+                backgroundColor = Color(0xFFC1A14E),
+                contentColor = Color.White
+            )
+        },
         bottomBar = {
             BottomNavigationBar(navController = navController)
         }
@@ -31,9 +66,72 @@ fun HomeScreen(
             modifier = Modifier
                 .padding(it)
                 .fillMaxSize(),
-            contentAlignment = Alignment.Center
         ) {
-            Text("Home Screen\n${sampleRepository.getMessage()}")
+            Image(
+                modifier = Modifier
+                    .blur(3.dp)
+                    .fillMaxSize(),
+                painter = painterResource(Res.drawable.room_sunny),
+                contentDescription = "background",
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                Box(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                        .background(Color.White)
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                ) {
+                    Column {
+                        Text(
+                            modifier = Modifier.padding(bottom = 8.dp),
+                            text = "2024/08/05 ~ 2024/08/11",
+                            style = TextStyle(
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        )
+                        Text("平均睡眠時間：　8時間")
+                        Text("平均カフェイン摂取量：　500mg")
+                    }
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End,
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .padding(end = 16.dp)
+                            .clip(RoundedCornerShape(16.dp))
+                            .width(120.dp)
+                            .background(Color.White)
+                            .padding(horizontal = 8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text("状態：　")
+                        Box(
+                            modifier = Modifier.size(80.dp),
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            Text("😀", fontSize = 24.sp)
+                        }
+                    }
+                }
+
+            }
+            Image(
+                modifier = Modifier
+                    .scale(1.5f)
+                    .padding(bottom = 100.dp)
+                    .align(Alignment.BottomCenter),
+                painter = painterResource(Res.drawable.character_good),
+                contentDescription = "character",
+            )
         }
     }
 }
