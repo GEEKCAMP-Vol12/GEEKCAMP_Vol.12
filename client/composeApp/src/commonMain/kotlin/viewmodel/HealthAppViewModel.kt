3d0fd5815d6fdad44d9900bea0f9300a35f00899
SEKI-YUTA <zard.since1991@gmail.com>
2024-08-09 01:05:31 +0900

    HealthAppViewModelを作成

---
+++ client/composeApp/src/commonMain/kotlin/viewmodel/HealthAppViewModel.kt
@@ -0,0 +1,64 @@
+package viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import data.demoCaffeineData
+import data.demoSleepData
+import data.demoUser
+import data.model.Caffeine
+import data.model.Lce
+import data.model.Sleep
+import data.model.User
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class HealthAppViewModel : ViewModel() {
+    private val _appViewState = MutableStateFlow<Lce<AppViewState>>(Lce.Loading)
+    val appViewState = _appViewState.asStateFlow()
+
+    fun fetchApplicationData() {
+        viewModelScope.launch {
+            val userDeferred = async { fetchUserData() }
+            val sleepDeferred = async { fetchCurrentWeekSleepData() }
+            val caffeineDeferred = async { fetchCurrentWeekCaffeineData() }
+
+            val user = userDeferred.await()
+            val sleepData = sleepDeferred.await()
+            val caffeineData = caffeineDeferred.await()
+
+            _appViewState.value = Lce.Content(
+                AppViewState(
+                    isLoading = false,
+                    currentUserData = user,
+                    currentWeekSleepData = sleepData,
+                    currentWeekCaffeineData = caffeineData
+                )
+            )
+        }
+    }
+
+    private suspend fun fetchCurrentWeekSleepData(): List<Sleep> {
+        delay(3_000)
+        return demoSleepData.take(7)
+    }
+
+    private suspend fun fetchCurrentWeekCaffeineData(): List<Caffeine> {
+        delay(3_500)
+        return demoCaffeineData.take(7)
+    }
+
+    private suspend fun fetchUserData(): User {
+        delay(2_000)
+        return demoUser
+    }
+}
+
+data class AppViewState(
+    val isLoading: Boolean = true,
+    val currentUserData: User? = null,
+    val currentWeekSleepData: List<Sleep>? = null,
+    val currentWeekCaffeineData: List<Caffeine>? = null,
+)
