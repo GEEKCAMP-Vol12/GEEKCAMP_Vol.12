51737a20dacf14993f7da939e70dd3ec1d90b693
SEKI-YUTA <zard.since1991@gmail.com>
2024-08-09 01:06:04 +0900

    HealthAppViewModelを依存関係注入のモジュールに追加

--- client/composeApp/src/commonMain/kotlin/di/AppModule.kt
+++ client/composeApp/src/commonMain/kotlin/di/AppModule.kt
@@ -1,11 +1,17 @@
 package di
 
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.bind
 import org.koin.dsl.module
+import viewmodel.HealthAppViewModel
 
 val appModule = module {
     single {
         SampleRepositoryImpl()
     }.bind<SampleRepository>()
+
+    viewModel {
+        HealthAppViewModel()
+    }
 }
 
